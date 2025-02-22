6334619a2e16224d30e5ab391962c1e58f711efb
ronfl3x <toledano.gab@gmail.com>
2025-02-22 10:00:40 +0100

    [TECH] correctly created the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div>
+      <p className=''>coucou</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@utils': path.resolve(__dirname, './src/lib/utils'),
+    },
+  },
+});
